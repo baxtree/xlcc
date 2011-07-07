b3120669d2283e189d57f139bba9ad407c9b6daa
baxtree <baxtree@163.com>
2011-07-07 03:53:18 +0100

    migrating okbuzzer to node.js

---
+++ node-xlcc/xlcc.js
@@ -0,0 +1,45 @@
+var xlcc = function(){
+	this.jid = "";
+};
+
+xlcc.logon = function(username, password){
+	
+};
+
+xlcc.logout = function(){
+	
+};
+
+xlcc.loadRemoteIM = function(url){
+	
+};
+
+xlcc.loadLocalIM = function(fpath){
+	
+};
+
+xlcc.compileIM = function(src){
+	
+};
+
+xlcc.loadRemoteOKC = function(url){
+	
+};
+
+xlcc.loadLocalOKC = function(fpath){
+	
+};
+
+xlcc.compileOKC = function(src){
+	
+};
+
+xlcc.loadHeader = function(head){
+	
+};
+
+xlcc.plays = function(jid, roleName){
+	
+};
+
+exports.xlcc = xlcc;
